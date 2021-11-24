6973ac758ba6fd50979d098b9f1d30f1d07d9825
Asus <you@gmail.com>
2021-11-24 22:53:32 +1000

    add example_1_squade2

---
+++ Example_1_squade2/Program.cs
@@ -0,0 +1,12 @@
+﻿// По двум заданным числам проверять является ли первое квадратом второго
+
+double numberA = 9;
+double numberB = 3;
+if (Math.Sqrt(numberA) == numberB)  //Math.Sqrt(Double)	- Возвращает квадратный корень из указанного числа.
+{
+    Console.WriteLine("Является");
+}
+else 
+{
+    Console.WriteLine("Не является");
+}
