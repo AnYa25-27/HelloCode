fe35e51117c1c55621090856010b26434f75df9d
Asus <you@gmail.com>
2021-11-24 22:23:17 +1000

    add example_0_square

---
+++ Example_0_square/Program.cs
@@ -0,0 +1,6 @@
+﻿// Вывести квадрат числа.
+
+Console.Write("Введите число: ");
+double numberA = double.Parse(Console.ReadLine()); // double.Parse(Console.ReadLine()); //Convert.ToDouble(Console.ReadLine());
+double result = numberA * numberA;
+Console.WriteLine (result);
