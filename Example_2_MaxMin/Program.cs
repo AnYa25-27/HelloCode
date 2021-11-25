34cfb2e0124395df978584aa92f11b9faff493ba
Asus_Ann <you@gmail.com>
2021-11-25 11:43:35 +1000

    add example 2 Max and Min

---
+++ Example_2_MaxMin/Program.cs
@@ -0,0 +1,21 @@
+﻿// Даны два числа. Показать большее и меньшее число
+
+double numberA = 25;
+double numberB = 196;
+
+if (numberA > numberB)
+{
+    Console.Write(numberA);
+    Console.WriteLine(" - большее число");
+
+    Console.Write(numberB);
+    Console.WriteLine(" - меньшее число");
+}
+else
+{
+    Console.Write(numberB);
+    Console.WriteLine(" - большее число");
+
+    Console.Write(numberA);
+    Console.WriteLine(" - меньшее число");
+}
