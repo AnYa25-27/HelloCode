46454caae04a6a55af19e7c8139761eb36104240
Asus <you@gmail.com>
2021-11-25 21:44:41 +1000

    add Example 3 week 2

---
+++ Example_3_week2/Program.cs
@@ -0,0 +1,13 @@
+﻿string[] weekDays = {"понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье"};
+Console.Write("Введите номер дня недели: ");
+int number = Convert.ToInt16(Console.ReadLine());
+
+if ( number < 8)
+if ( number > 0)
+{
+    Console.WriteLine($"Сегодня {weekDays[number-1]}");
+}
+else
+{
+    Console.WriteLine("Вы ввели неверный день недели");
+}
