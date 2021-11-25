ab485461cfafc256223b010e4f9cadbab36a2066
Asus <you@gmail.com>
2021-11-25 22:55:41 +1000

    add example 4 max Of Three

---
+++ Example_4_maxOfThree/Program.cs
@@ -0,0 +1,11 @@
+﻿double numberA = new Random().Next();
+double numberB = new Random().Next();
+double numberC = new Random().Next();
+
+double max = numberA;
+
+if (numberA > max) max = numberA;
+if (numberB > max) max = numberB;
+if (numberC > max) max = numberC;
+
+Console.WriteLine($"Максимальное значение {max} ");
