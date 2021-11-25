fa19e14d816da298850fb60730f34ac5f2d7b5e0
Asus_Ann <you@gmail.com>
2021-11-25 15:54:19 +1000

    add example 3 week

---
+++ Example_3_week/Program.cs
@@ -0,0 +1,16 @@
+﻿//
+
+Console.Write("Введите номер дня недели: ");
+int day = Convert.ToInt16(Console.ReadLine()); //Convert.ToInt16(Console.ReadLine());
+switch (day)
+{
+    case 1: Console.WriteLine("Понедельник"); break;
+    case 2: Console.WriteLine("Вторник");break;
+    case 3: Console.WriteLine("Среда"); break;
+    case 4: Console.WriteLine("Четверг"); break;
+    case 5: Console.WriteLine("Пятница"); break;
+    case 6: Console.WriteLine("Суббота"); break;
+    case 7: Console.WriteLine("Воскресенье");break;
+ default: Console.WriteLine("Ошибка, вы ввели неверное число!");break;
+}
+Console.Write(day);
